92bcb4cf3674a0740dec883dd455c6951cb9f2e0
Mylo <yingboming@worktrans.cn>
2022-04-26 23:19:20 +0800

    442

---
+++ 442.数组中重复的数据.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=442 lang=javascript
+ *
+ * [442] 数组中重复的数据
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+// 不满足时间复杂度为 O(n) 的要求
+// var findDuplicates = function (nums) {
+//   const n = nums.length,
+//     twins = new Map(),
+// 		ans =[]
+
+//   for (const value of nums) {
+//     if (!twins.has(value)) {
+//       twins.set(value, 1);
+//     } else {
+//       twins.set(value, twins.get(value) + 1);
+//     }
+// 		if(twins.get(value) > 1) {
+// 			ans.push(value)
+
+// 		}
+//   }
+
+// 	return ans
+// };
+
+/**
+ * @param {number[]} nums
+ * 由于 1 <= nums[i] <= n，所以 nums[i]-1 可以被视为一个索引
+ * （索引从 0 开始，最小值为 1， 所以减去 1）
+ * 如果值为正数，就原地修改该索引对应的值为负数
+ * 如果值为负数，说明已经出现过，即重复的数字
+ *  */ 
+var findDuplicates = function (nums) {
+  const twins = [];
+  for (let i = 0; i < nums.length; i++) {
+    let index = Math.abs(nums[i]) - 1;
+    if (nums[index] < 0) {
+      twins.push(Math.abs(nums[i]));
+    } else {
+      nums[index] = -nums[index];
+    }
+  }
+  return twins;
+};
+
+// @lc code=end
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # leetcode
-my leetcode practice
+my leetcode practice log
