24c816e7b1f92a482b9a2ac522e0e06d0e5c5aca
Mylo <yingboming@worktrans.cn>
2022-05-31 23:26:07 +0800

    104 111 144 🌲️

---
+++ 104.二叉树的最大深度.js
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ *
+ * https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (76.97%)
+ * Likes:    1259
+ * Dislikes: 0
+ * Total Accepted:    741.7K
+ * Total Submissions: 963.3K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ * 返回它的最大深度 3 。
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+	if (!root) return 0;
+
+	const left = maxDepth(root.left);
+	const right = maxDepth(root.right);
+
+	return Math.max(left, right) + 1;
+};
+// @lc code=end
---
+++ 111.二叉树的最小深度.js
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=111 lang=javascript
+ *
+ * [111] 二叉树的最小深度
+ *
+ * https://leetcode.cn/problems/minimum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (50.29%)
+ * Likes:    768
+ * Dislikes: 0
+ * Total Accepted:    399.2K
+ * Total Submissions: 791.3K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最小深度。
+ *
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ *
+ * 说明：叶子节点是指没有子节点的节点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：2
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [2,null,3,null,4,null,5,null,6]
+ * 输出：5
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数的范围在 [0, 10^5] 内
+ * -1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function (root) {
+	if (!root) return 0;
+
+	const left = minDepth(root.left);
+	const right = minDepth(root.right);
+
+	// 其中最小深度必有一个为 0 时，返回其和加 1
+	if (root.left === null || root.right === null) return left + right + 1;
+
+	// 左右都不为 0 时
+	return Math.min(left, right) + 1;
+};
+// @lc code=end
