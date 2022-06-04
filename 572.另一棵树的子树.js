7fdf3ec40d4f10f04fafca2a71f6c23626f18783
Mylo <yingboming@worktrans.cn>
2022-06-04 23:45:14 +0800

    572 另一颗树的子树

---
+++ 572.另一棵树的子树.js
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=javascript
+ *
+ * [572] 另一棵树的子树
+ *
+ * https://leetcode.cn/problems/subtree-of-another-tree/description/
+ *
+ * algorithms
+ * Easy (47.55%)
+ * Likes:    743
+ * Dislikes: 0
+ * Total Accepted:    126.6K
+ * Total Submissions: 265.9K
+ * Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
+ *
+ *
+ *
+ * 给你两棵二叉树 root 和 subRoot 。检验 root 中是否包含和 subRoot 具有相同结构和节点值的子树。如果存在，返回 true
+ * ；否则，返回 false 。
+ *
+ * 二叉树 tree 的一棵子树包括 tree 的某个节点和这个节点的所有后代节点。tree 也可以看做它自身的一棵子树。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [3,4,5,1,2], subRoot = [4,1,2]
+ * 输出：true
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+ * 输出：false
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * root 树上的节点数量范围是 [1, 2000]
+ * subRoot 树上的节点数量范围是 [1, 1000]
+ * -10^4
+ * -10^4
+ *
+ *
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function (root, subRoot) {
+	// 100. 是否相同的树🌲️
+	const isSameTree = (p, q) => {
+		if (p === null && q === null) return true;
+		if (p === null || q === null) return false;
+		if (p.val !== q.val) return false;
+
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	};
+
+	if (root === null) return subRoot === null;
+
+	if (isSameTree(root, subRoot)) return true;
+
+	return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
+// @lc code=end
