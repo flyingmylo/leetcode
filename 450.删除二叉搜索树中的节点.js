11dca16a18e718c676d81fa35e380a8579dc2cbf
Mylo <yingboming@worktrans.cn>
2022-07-15 18:27:59 +0800

    450 删除bst中节点

---
+++ 450.删除二叉搜索树中的节点.js
@@ -0,0 +1,116 @@
+/*
+ * @lc app=leetcode.cn id=450 lang=javascript
+ *
+ * [450] 删除二叉搜索树中的节点
+ *
+ * https://leetcode.cn/problems/delete-node-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (51.97%)
+ * Likes:    905
+ * Dislikes: 0
+ * Total Accepted:    145.9K
+ * Total Submissions: 279.9K
+ * Testcase Example:  '[5,3,6,2,4,null,7]\n3'
+ *
+ * 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key
+ * 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。
+ *
+ * 一般来说，删除节点可分为两个步骤：
+ *
+ *
+ * 首先找到需要删除的节点；
+ * 如果找到了，删除它。
+ *
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ *
+ *
+ * 输入：root = [5,3,6,2,4,null,7], key = 3
+ * 输出：[5,4,6,2,null,null,7]
+ * 解释：给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+ * 一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+ * 另一个正确答案是 [5,2,6,null,4,null,7]。
+ *
+ *
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: root = [5,3,6,2,4,null,7], key = 0
+ * 输出: [5,3,6,2,4,null,7]
+ * 解释: 二叉树不包含值为 0 的节点
+ *
+ *
+ * 示例 3:
+ *
+ *
+ * 输入: root = [], key = 0
+ * 输出: []
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 节点数的范围 [0, 10^4].
+ * -10^5 <= Node.val <= 10^5
+ * 节点值唯一
+ * root 是合法的二叉搜索树
+ * -10^5 <= key <= 10^5
+ *
+ *
+ *
+ *
+ * 进阶： 要求算法时间复杂度为 O(h)，h 为树的高度。
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+//  获取最小树
+const getMin = (root) => {
+	while (root.left !== null) root = root.left
+	return root
+}
+var deleteNode = function (root, key) {
+	if (!root) return null
+
+	if (root.val === key) {
+		if (root.left === null) return root.right
+		if (root.right === null) return root.left
+
+		// 获取右子树中最小的节点
+		let minNode = getMin(root.right)
+		// 删除右子树中最小的节点
+		root.right = deleteNode(root.right, minNode.val)
+		// 替换 root
+		minNode.left = root.left
+		minNode.right = root.right
+		root = minNode
+	} else if (root.val > key) {
+		root.left = deleteNode(root.left, key)
+	} else if (root.val < key) {
+		root.right = deleteNode(root.right, key)
+	}
+
+	return root
+}
+// @lc code=end
